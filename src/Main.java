c272311fa42687bd11dc5dbc11f89a46c57b43aa
maxdd <maxizi1983@gmail.com>
2024-08-26 17:24:45 +0500

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 8;
-        int b = 8;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
         int c = a + b;
-        if( a < b){
+        System.out.println("Sum: " + c);
+
+        if (a < b) {
             c = b - a;
+            System.out.println("Difference (b - a): " + c);
+        } else if (a > b) {
+            c = a - b;
+            System.out.println("Difference (a - b): " + c);
+        } else {
+            System.out.println("Numbers are equal, difference is 0");
         }
-        System.out.println(c);
+
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        if (b != 0) {
+            double quotient = (double) a / b;
+            System.out.println("Quotient (a / b): " + quotient);
+        } else {
+            System.out.println("Cannot divide by zero");
+        }
+
+        scanner.close();
     }
 }
